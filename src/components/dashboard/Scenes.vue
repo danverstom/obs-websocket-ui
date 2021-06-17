3d0ca36d70d99f69f6198ddfc848475137c253e8
Tom <tomtomgaming101@gmail.com>
2021-06-17 18:32:15 +0100

    created scenes component

--- src/components/dashboard/Scenes.vue
+++ src/components/dashboard/Scenes.vue
@@ -10,10 +10,51 @@
 </template>
 
 <script>
-import Scene from "../components/dashboard/Scene.vue";
+import Scene from "@/components/dashboard/Scene.vue";
 
 export default {
-  
-}
+  name: "Scenes",
+  components: {
+    Scene,
+  },
+  props: {
+    obs: Object,
+  },
+  data() {
+    return {
+      scene_data: [],
+      current_scene: "",
+      ui_selected_scene: "",
+    }
+  },
+  created() {
+    this.obs.on("SwitchScenes", (data) => {
+      console.log("Scene change detected");
+      console.log(data);
+      this.queryScenes();
+    });
 
+    this.queryScenes();
+  },
+  methods: {
+    async queryScenes() {
+      const scene_data = await this.obs.send("GetSceneList");
+      console.log(scene_data);
+      this.scene_data = scene_data;
+      this.current_scene = scene_data.currentScene;
+      this.ui_selected_scene = scene_data.currentScene;
+
+      console.log(`Current Scene: ${this.current_scene}`)
+      console.log(`UI Selected Scene: ${this.ui_selected_scene}`)
+
+    },
+    changeScene(name) {
+      console.log("changing scene to " + name)
+      this.ui_selected_scene = name
+      this.obs.send("SetCurrentScene", {
+        "scene-name": name
+      })
+    }
+  }
+};
 </script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -11,14 +11,7 @@
           v-if="!obs"
         />
         <div v-if="obs">
-          <div v-for="scene in scene_data.scenes" :key="scene.name">
-            <Scene
-              :name="scene.name"
-              :current_scene="current_scene"
-              :ui_selected_scene="ui_selected_scene"
-              v-on:changeScene="changeScene"
-            />
-          </div>
+          <Scenes :obs="obs" />
         </div>
       </div>
     </div>
@@ -26,54 +19,23 @@
 </template>
 
 <script>
-import Scene from "../components/dashboard/Scene.vue";
+import Scenes from "../components/dashboard/Scenes.vue";
 import Connect from "../components/dashboard/Connect.vue";
 
 export default {
   name: "Dashboard",
   components: {
-    Scene,
+    Scenes,
     Connect,
   },
   methods: {
     async acceptConnection(obs) {
       this.obs = obs;
       console.log(this.obs);
-
-      // Register events to listen to
-
-      this.obs.on("SwitchScenes", (data) => {
-        console.log("Scene change detected")
-        console.log(data)
-        this.queryScenes()
-      })
-
-      this.queryScenes();
-    },
-    changeScene(name) {
-      console.log("changing scene to " + name)
-      this.ui_selected_scene = name
-      this.obs.send("SetCurrentScene", {
-        "scene-name": name
-      })
-    },
-    async queryScenes() {
-      const scene_data = await this.obs.send("GetSceneList");
-      console.log(scene_data);
-      this.scene_data = scene_data;
-      this.current_scene = scene_data.currentScene;
-      this.ui_selected_scene = scene_data.currentScene;
-
-      console.log(`Current Scene: ${this.current_scene}`)
-      console.log(`UI Selected Scene: ${this.ui_selected_scene}`)
-
     }
   },
   data() {
     return {
-      scene_data: [],
-      current_scene: "",
-      ui_selected_scene: "",
       obs: false,
     };
   },
